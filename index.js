d1c7b2f62ea544e93d8b69f795d2d5f307eda03c
MeriJokhadze <jokhadzemeri1@gmail.com>
2023-09-28 14:48:47 +0400

    add media and burger

--- index.js
+++ index.js
@@ -62,3 +62,22 @@ function removeActive() {
         panel.classList.remove('active');
     })
 }
+
+// BURGER BAR 
+
+let burgerBar = document.getElementById('burger');
+let line1 = document.querySelector('.line1');
+let line3 = document.querySelector('.line3');
+let line2 = document.querySelector('.line2');
+let line = document.querySelector('.line');
+// burgerBar.addEventListener('click', function(){
+//     navBar.classList.toggle('activeNav');
+
+// });
+
+burgerBar.addEventListener('click', function(){
+    line1.classList.toggle('line1Active');
+    line3.classList.toggle('line3Active');
+    line2.classList.toggle('line2NO');
+    line.classList.toggle('lineAc');
+})
